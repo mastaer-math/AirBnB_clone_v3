bb158e93749bae80947745d51c6c017ea3620fe1
kiminzajnr <kiminzaeric@gmail.com>
2021-09-20 05:57:46 +0000

    try to fix check 7

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,24 +20,23 @@ states_ = storage.count(State)
 users_ = storage.count(User)
 
 
+data = {"amenities": amenities_,
+        "cities": cities_,
+        "places": places_,
+        "reviews": reviews_,
+        "states": states_,
+        "users": users_
+        }
+
+
 @app_views.route('/status', strict_slashes=False)
 def return_json():
     """return a JSON: 'status': 'OK'"""
-    data = {"status": "OK"}
-    return jsonify(data)
+    status = {"status": "OK"}
+    return jsonify(status)
 
 
 @app_views.route('/stats', strict_slashes=False)
 def obj_no():
     """retrieves the number of each objects by type"""
-    data = {"amenities": amenities_,
-            "cities": cities_,
-            "places": places_,
-            "reviews": reviews_,
-            "states": states_,
-            "users": users_
-            }
     return jsonify(data)
-
-if __name__ == "__main__":
-    pass
