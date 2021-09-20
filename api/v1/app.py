65367f301251e701e2b50e0d7ff6c829c418e646
kiminzajnr <kiminzaeric@gmail.com>
2021-09-20 06:17:18 +0000

    Not found handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,9 @@ start of my API, work towards endpoint(route) to return
 status of my application
 """
 from os import getenv
-from flask import Flask
+
+from flask import Flask, make_response, jsonify
+
 from models import storage
 from api.v1.views import app_views
 
@@ -17,6 +19,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_storage(error=None):
     """calls storage.close()"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
